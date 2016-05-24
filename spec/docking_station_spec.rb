001b52afbbd98e57a51074fd2d23933b908b81f8
Alex-Swann <alex_swann@hotmail.co.uk>
2016-05-24 14:47:49 +0100

    added docking station class and release bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,9 @@
-
+ require 'docking_station'
 
 
 describe DockingStation do
-  #it 'is a DockingStation'
+  it 'needs to release bike' do
+    docking_station = DockingStation.new
+    expect(docking_station.respond_to?(:release_bike)).to eq true
+  end
 end
