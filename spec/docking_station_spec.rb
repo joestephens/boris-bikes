5ee9ae364614a98322ea488f6ceec30c57d88e9a
joestephens <joe.stephens@live.co.uk>
2016-05-24 17:54:32 +0100

    Changed needs to responds to

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -13,21 +13,21 @@ describe DockingStation do
     expect(subject.bikes.class).to eq Fixnum
   end
 
-  it 'needs to release bike' do
+  it 'responds to release bike' do
     expect(subject.respond_to?(:release_bike)).to eq true
   end
 
-    it 'releases a working bike' do
-      subject.dock(1)
-      expect(bike).to be_working
-      expect(subject.release_bike.class).to eq Fixnum
-    end
+  it 'releases a working bike' do
+    subject.dock(1)
+    expect(bike).to be_working
+    expect(subject.release_bike.class).to eq Fixnum
+  end
 
-    it 'should dock bike' do
-      expect(subject.dock(bike)).to eq bike
-    end
+  it 'should dock bike' do
+    expect(subject.dock(bike)).to eq bike
+  end
 
-    it "doesn't release a bike if station empty" do
-      expect{subject.release_bike}.to raise_error 'No bikes available!'
-    end
+  it 'doesn\'t release a bike if station empty' do
+    expect{subject.release_bike}.to raise_error 'No bikes available!'
+  end
 end
