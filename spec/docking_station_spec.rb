dffb2cc243ec0232fe10fb8898a8f43141868fa1
Alex-Swann <alex_swann@hotmail.co.uk>
2016-05-24 14:52:56 +0100

    added bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,17 @@
- require 'docking_station'
+require 'docking_station'
+require 'bike'
 
 
 describe DockingStation do
   it 'needs to release bike' do
     docking_station = DockingStation.new
     expect(docking_station.respond_to?(:release_bike)).to eq true
+  end
+end
+
+describe Bike do
+  it 'needs to be working' do
+    bike = Bike.new
+    expect(bike.respond_to?(:working?)).to eq true
   end
 end
