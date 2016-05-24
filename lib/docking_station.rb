7378435f208ff562d8ecf408978c972b3677c4ed
Alex-Swann <alex_swann@hotmail.co.uk>
2016-05-24 17:42:55 +0100

    raise error no bike test added

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,13 +2,14 @@ require_relative 'bike'
 
 class DockingStation
   attr_reader :bikes
-  
+
   def initialize
     @bikes = 0
   end
 
   def release_bike
-    Bike.new
+    raise "No bikes available!" unless @bikes > 0
+    @bikes -= 1
   end
 
   def dock(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,20 +9,25 @@ describe DockingStation do
     expect(bike.respond_to?(:working?)).to eq true
   end
 
+  it 'shows bikes' do
+    expect(subject.bikes.class).to eq Fixnum
+  end
+
   it 'needs to release bike' do
     expect(subject.respond_to?(:release_bike)).to eq true
   end
 
-  it 'releases a working bike' do
-    expect(bike).to be_working
-    expect(subject.release_bike.class).to eq Bike
-  end
+    it 'releases a working bike' do
+      subject.dock(1)
+      expect(bike).to be_working
+      expect(subject.release_bike.class).to eq Fixnum
+    end
 
-  it 'should dock bike' do
-    expect(subject.dock(bike)).to eq bike
-  end
+    it 'should dock bike' do
+      expect(subject.dock(bike)).to eq bike
+    end
 
-  it 'shows bikes' do
-    expect(subject.bikes.class).to eq Fixnum
-  end
+    it "doesn't release a bike if station empty" do
+      expect{subject.release_bike}.to raise_error 'No bikes available!'
+    end
 end
