a7e3dcd90f12df9be46f9232b13e49919f7948f7
Alex-Swann <alex_swann@hotmail.co.uk>
2016-05-24 16:32:26 +0100

    refactored instantiation of docking station with keyword subject

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,22 +1,20 @@
 require 'docking_station'
 require 'bike'
 
-describe Bike do
+
+describe DockingStation do
+
   bike = Bike.new
   it 'needs to be working' do
     expect(bike.respond_to?(:working?)).to eq true
   end
-end
 
-describe DockingStation do
   it 'needs to release bike' do
-    docking_station = DockingStation.new
-    expect(docking_station.respond_to?(:release_bike)).to eq true
+    expect(subject.respond_to?(:release_bike)).to eq true
   end
+
   it 'receives a working bike' do
-    bike = Bike.new
-    docking_station = DockingStation.new
     expect(bike).to be_working
-    expect(docking_station.release_bike.class).to eq Bike
+    expect(subject.release_bike.class).to eq Bike
   end
 end
