f4c03bbd6cdc615ccb1ea89b685ccdd8d209aa35
Luke <lukeaveil@yahoo.com>
2016-05-26 09:59:28 +0100

    docking station code updated

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,6 +1,7 @@
 require_relative 'bike'
 
 class DockingStation
+	
 	attr_reader :bikes
 
 	def initialize
@@ -10,7 +11,7 @@ class DockingStation
 	def release_bike
 		fail "There are no bikes" if @bikes.length == 0
 		@bikes.pop
-		
+
 	end
 
 	def dock (bike)
